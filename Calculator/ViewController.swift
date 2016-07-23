e19270c91c3d959a119e877589f297dd821a80ad
José Gutiérrez <guti7@users.noreply.github.com>
2016-07-23 07:50:24 -0700

    initial commit
    - button
    - and action for button

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,17 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    // MARK: Actions
+    
+    @IBAction func touchDigit(sender: UIButton) {
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
 }
 
